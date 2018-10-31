e12f1b38b62e2cebc6183ef160a784739ef653ba
Laura Shamus <lkshamus@gmail.com>
2018-10-31 16:39:48 -0600

    Initial set up installing dev dependencies

---
+++ commuter/src/setupTests.js
@@ -0,0 +1,30 @@
+Timport { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+class LocalStorage{
+  constructor(){
+    this.store = {};
+  }
+
+  getItem = (key) => {
+    return this.store[key]
+  }
+
+  setItem = (key, value) => {
+    this.store[key] = value;
+  }
+
+  removeItem = (key) => {
+    delete this.store[key];
+    console.log('fires')
+  }
+
+  clear = () => {
+    this.store = {}
+  }
+
+}
+
+export default LocalStorage;
